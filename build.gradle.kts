eb4869c900dad31bfdb065ed736ce828e87306ee
tioma-master <tioma.master@gmail.com>
2022-01-19 20:37:50 +0200

    Upgrade gradle version. Update project sturcture and build scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,25 @@
-val coroutinesVersion = "1.6.0-RC"
-val serializationVersion = "1.3.1"
-
-plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
-    id("com.android.library")
-}
-
-repositories {
-    google()
-    mavenCentral()
-}
-
-kotlin {
-    android()
-    ios {
-        binaries {
-            framework {
-                baseName = "esp-idf-provisioning"
-            }
-        }
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$serializationVersion")
-                implementation("io.github.aakira:napier:2.1.0")
-                implementation("com.juul.kable:core:0.11.0")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutinesVersion}")
-                implementation("com.google.crypto.tink:tink-android:1.6.1")
-            }
-        }
-        val androidTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13.1")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
-            }
-        }
-        val iosMain by getting
-        val iosTest by getting
+
+    val kotlinVersion = "1.6.10"
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
+        classpath(kotlin("serialization", version = kotlinVersion))
+        classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
 
-android {
-    compileSdk = 31
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 31
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,23 +5,6 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "esp-idf-provisioning"
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-
-    val kotlinVersion = "1.6.10"
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = kotlinVersion))
-        classpath(kotlin("serialization", version = kotlinVersion))
-        classpath("com.android.tools.build:gradle:7.0.4")
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
+include(":shared")
